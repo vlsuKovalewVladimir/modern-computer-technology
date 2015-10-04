8b195c9eefe236e8a9e0911c6dbd90a627baf927
Ковалев Владимир Евгеньевич <kovalew.vladimir@gmail.com>
2015-10-04 17:28:03 +0300

    Создал проект для лабораторной работы №2

---
+++ lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
