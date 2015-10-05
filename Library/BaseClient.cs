af2e747c065aa7fe8fe9962fadac76f453077c2f
Ковалев Владимир Евгеньевич <kovalew.vladimir@gmail.com>
2015-10-05 13:25:18 +0300

    Добавил базовую реализацию клиента Library.BaseClient

---
+++ Library/BaseClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Library
+{
+    public abstract class BaseClient : IClient
+    {
+        public IPEndPoint ipEndPoint { get; set; }
+
+        public BaseClient() { }
+
+        public BaseClient(IPEndPoint ipEndPoint)
+        {
+            this.ipEndPoint = ipEndPoint;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                TcpClient sender = new TcpClient();
+                sender.Connect(ipEndPoint);
+                SendMessageRemoteServer(sender);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Ошибка: {0}", e.Message);
+                System.Diagnostics.Debug.WriteLine(e.StackTrace);
+            }
+        }
+
+        public abstract void SendMessageRemoteServer(TcpClient sender);
+    }
+}
--- lab1/Client.cs
+++ lab1/Client.cs
@@ -7,35 +7,26 @@ using Library;
 
 namespace lab1
 {
-    public class Client : IClient
+    public class Client : BaseClient
     {
-        public IPEndPoint ipEndPoint { get; set; }
+        public Client() : base() { }
+        public Client(IPEndPoint ipEndPoint) : base(ipEndPoint) { }
 
-        public Client() { }
-
-        public Client(IPEndPoint ipEndPoint)
+        public override void SendMessageRemoteServer(TcpClient sender)
         {
-            this.ipEndPoint = ipEndPoint;
-        }
-
-        public void Start()
-        {
-            TcpClient sender = new TcpClient();
-            sender.Connect(ipEndPoint);
-
             Console.WriteLine("Подключился к серверу: {0}", sender.Client.RemoteEndPoint.ToString());
 
             NetworkStream ns = sender.GetStream();
             StreamWriter sw = new StreamWriter(ns);
             StreamReader sr = new StreamReader(ns);
-            
+
             while (sender.Connected)
             {
                 Console.Write("Введите сообщение: ");
                 string message = Console.ReadLine();
                 sw.WriteLine(message);
                 sw.Flush();
-                
+
                 if (message == "end")
                 {
                     sw.Close();
@@ -43,6 +34,6 @@ namespace lab1
                     sender.Close();
                 }
             }
-        }    
+        }
     }
 }
